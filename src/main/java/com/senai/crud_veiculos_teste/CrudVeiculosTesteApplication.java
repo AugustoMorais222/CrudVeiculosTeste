b2bf010a78d87d2491af335a46f627f86f7fde8e
AugustoMorais222 <augustodemor04@gmail.com>
2025-10-21 19:20:53 -0300

    first commit

---
+++ src/main/java/com/senai/crud_veiculos_teste/CrudVeiculosTesteApplication.java
@@ -0,0 +1,13 @@
+package com.senai.crud_veiculos_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVeiculosTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudVeiculosTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/crud_veiculos_teste/CrudVeiculosTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.crud_veiculos_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVeiculosTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
