7a5f3f591284ca6c0609133ae02289cccb6dffd0
AugustoMorais222 <augustodemor04@gmail.com>
2025-10-21 20:59:14 -0300

    Adicionando view

--- src/main/java/com/senai/crud_veiculos_teste/controller/VeiculoController.java
+++ src/main/java/com/senai/crud_veiculos_teste/controller/VeiculoController.java
@@ -1,7 +1,6 @@
 package com.senai.crud_veiculos_teste.controller;
 
 import com.senai.crud_veiculos_teste.entidades.Veiculo;
-import com.senai.crud_veiculos_teste.repositorio.VeiculoRepositorio;
 import com.senai.crud_veiculos_teste.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,34 +8,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-
 @RestController
 @RequestMapping("/api/veiculo")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class VeiculoController {
 
     @Autowired
-    public VeiculoService veiculoService;
+    private VeiculoService veiculoService;
 
     @GetMapping
     public List<Veiculo> findAll() {
         return veiculoService.findAll();
     }
 
-    @PostMapping
-    public Veiculo create(Veiculo veiculo) {
-        return veiculoService.create(veiculo);
-    }
-    
-    @PutMapping
-    public Veiculo update(Veiculo veiculo) {
-        return veiculoService.update(veiculo);
-    }
     @GetMapping("/{id}")
     public Veiculo findById(@PathVariable UUID id) {
         return veiculoService.findById(id);
     }
-    @DeleteMapping
+
+    @PostMapping
+    public Veiculo create(@RequestBody Veiculo veiculo) {
+        System.out.println("Criando veículo: " + veiculo);
+        return veiculoService.create(veiculo);
+    }
+
+    @PutMapping("/{id}")
+    public Veiculo update(@PathVariable UUID id, @RequestBody Veiculo veiculo) {
+        veiculo.setId(id);
+        System.out.println("Atualizando veículo: " + veiculo);
+        return veiculoService.update(veiculo);
+    }
+
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable UUID id) {
         veiculoService.delete(veiculoService.findById(id));
     }
